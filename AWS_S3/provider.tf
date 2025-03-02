2942ed47f7f596a3aa8d9a1ddaf1264bb3a3ae48
tharun2258 <tharunk2258@gmail.com>
2025-03-02 13:08:52 +0530

    added terraform s3 files

--- AWS_S3/provider.tf
+++ AWS_S3/provider.tf
@@ -1,6 +1,6 @@
 terraform {
   required_providers {
-    aws ={
+    aws={
         source = "hashicorp/aws"
         version = "~> 5.2"
     }
@@ -10,4 +10,4 @@ terraform {
 provider "aws" {
     region = "us-east-1"
   
-}               
+}
