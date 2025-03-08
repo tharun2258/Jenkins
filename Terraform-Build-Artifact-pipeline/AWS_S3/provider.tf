e0203d0d6aff05c7eff825fcc7107fe2a8a80a16
tharun2258 <tharunk2258@gmail.com>
2025-03-08 14:04:42 +0530

    updated jenkins file in apply config

--- Terraform-Build-Artifact-pipeline/AWS_S3/provider.tf
+++ Terraform-Build-Artifact-pipeline/AWS_S3/provider.tf
@@ -9,6 +9,13 @@ terraform {
 
 
 
+terraform {
+  backend "s3" {
+    bucket         = "terraform-tfstate-bucket-demo"
+    key            = "AWS_S3/terraform.tfstate"
+    region         = "us-east-1"
+  }
+}
 
 
 provider "aws" {
