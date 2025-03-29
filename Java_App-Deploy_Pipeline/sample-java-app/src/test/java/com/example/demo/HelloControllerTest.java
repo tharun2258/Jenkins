12ef836414081baf898212e2ee017d65fd6c7937
tharun2258 <tharunk2258@gmail.com>
2025-03-29 20:26:46 +0530

    created java app jenkins pipeline

---
+++ Java_App-Deploy_Pipeline/sample-java-app/src/test/java/com/example/demo/HelloControllerTest.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class HelloControllerTest {
+    @Test
+    void sampleTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
